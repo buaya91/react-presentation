44ace4237f6b9db5ea8c5edd9998735feba6b965
qingwei <qingwei@binary.com>
2016-01-08 10:23:03 +0800

    init gh pages

--- src/presentation/Body.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by personal on 15/12/2015.
- */
-import React from 'react'
-
-export default ({content}) => {
-  return <h3>{content}</h3>
-}
--- src/presentation/Button.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by personal on 15/12/2015.
- */
-import React from 'react'
-
-export default ({text, onclick, autoFocus}) => {
-  return <button autoFocus={autoFocus} onClick={onclick}>{text}</button>
-}
